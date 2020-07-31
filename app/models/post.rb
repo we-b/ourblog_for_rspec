457164eb726901875ff2d786fc7714302ed278e1
shuntas <shunta.suzuki@di-v.co.jp>
2020-07-31 16:18:47 +0900

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :image, presence: true
+end
